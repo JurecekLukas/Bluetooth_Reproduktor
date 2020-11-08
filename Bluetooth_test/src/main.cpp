2a17e43912d41b52137d514e37ae98d9927a289e
JurecekLukas <lukas.jurecek@zsvelkeheraltice.cz>
2020-11-08 09:11:27 +0100

    kódy

---
+++ Bluetooth_test/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <BluetoothSerial.h>
+
+/*
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLEB)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+*/
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("LEDKA");
+  Serial.println("The device started.");
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  if (Serial.available()){
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()){
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
+  // put your main code here, to run repeatedly:
+}
