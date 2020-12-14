19a40fbe2a2e6bb3c3c88badfef2680c4b3c5b89
JurecekLukas <lukas.jurecek@zsvelkeheraltice.cz>
2020-12-14 17:30:48 +0100

    web+tlacitka

---
+++ ArduinoIDE/Wifirad_webTL/data/script.js
@@ -0,0 +1,25 @@
+document.getElementById('TL1').addEventListener('click', function(){
+    alert("Jedna");
+});
+document.getElementById('TL2').addEventListener('click',  function(){
+    alert("Dva");
+});
+document.getElementById('TL3').addEventListener('click', function(){
+    alert("Dři");
+});
+document.getElementById('TL4').addEventListener('click', function(){
+    alert("ŠTYRI");
+});
+document.getElementById('TL5').addEventListener('click', function(){
+    alert("Pět");
+});
+document.getElementById('TL6').addEventListener('click', function(){
+    alert("Šestka");
+});
+document.getElementById('TL7').addEventListener('click',  function(){
+    alert("Bzum bzum bzum bzum bzum bzum bzum");
+});
+document.getElementById('TL8').addEventListener('click',  function(){
+    alert("Osum");
+});
+
